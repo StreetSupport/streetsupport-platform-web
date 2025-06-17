db9ffaabf3ef3dd91b4e59dd10a17f3e8d09f1e5
JAMES-PC\jimmy <jimmy.cross@gmail.com>
2025-06-17 08:44:28 +0100

    exposes secret via next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,3 @@
-// next.config.ts
-
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env.local' });
 
@@ -7,6 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    MONGODB_URI: process.env.MONGODB_URI,
   },
 };
 
